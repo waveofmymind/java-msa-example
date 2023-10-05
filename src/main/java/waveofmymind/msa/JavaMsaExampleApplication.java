3498abbd1f3138f1da668448d70f0c6f4f097905
waveofmymind <sjun0913@gmail.com>
2023-10-05 16:57:26 +0900

    feat: 기본적인 프로젝트를 세팅한다.

---
+++ src/main/java/waveofmymind/msa/JavaMsaExampleApplication.java
@@ -0,0 +1,13 @@
+package waveofmymind.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMsaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/waveofmymind/msa/JavaMsaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package waveofmymind.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
